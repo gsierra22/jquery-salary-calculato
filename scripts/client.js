5d27dcf027d97def0124c17eefc73df8871c355e
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2021-09-13 17:35:28 -0500

    Week chall part5

--- scripts/client.js
+++ scripts/client.js
@@ -1,11 +1,11 @@
 $ (document).ready(onReady);
 
-let employees = []
-let monthSum= []
+let employees = []//input array
+
 function onReady(){
-  $('#submit').on('click', employeeChart)
-  $('#submit').on('click', calc)
-  $('#body').on('click', '.delete', deleteButton)
+  $('#submit').on('click', employeeChart)//input array
+  $('#submit').on('click', calc)//salary calculator
+  $('#body').on('click', '.delete', deleteButton)//delete button
 }
 
 
@@ -16,17 +16,17 @@ function employeeChart(){
     idNum: $('#idnum').val(),
     title: $('#title').val(),
     anSal: $('#ansal').val()
-  }
+  }//end object
 
   let blankety = false
   for (const hallo in emChart){
     if(!emChart[hallo]){
       blankety=true
-    }
-  }
+    }//end if
+  }// end for
   if (blankety ===true){
     alert('Please enter missing information')
-  }
+  }// end if
   else if(blankety===false){
     employees.push(emChart);
     let el =$('#body');
@@ -51,27 +51,27 @@ function employeeChart(){
   $('#idnum').val(''),
   $('#title').val(''),
     $('#ansal').val('')
-  }
-}
+  } //end else if
+}// end function
 
 function deleteButton (){
   $(this).closest("tr").remove();
-}
+} //end function
 
 function calc(){
   let sum=0
   for (let i=0; i<employees.length; i++){
     sum += Number(employees[i].anSal)
     console.log(sum)
-  }
+  }// end for loop
   let monthlySum=(sum/12)
   let calculator= $('#div3')
   $("#div3").css('background-color','yellow')
   calculator.empty()
   calculator.append(`<p>Total Monthly Salary:${monthlySum}</p>`)
 
-  if (monthlySum > 20000){
+  if (monthlySum >= 20000){
     $("#div3").css('background-color','red')
-  }
-}
+  }// end if
+}// end function
 
